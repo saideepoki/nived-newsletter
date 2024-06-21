d9097951715f094284054f59d70bf6fdfcda66b1
saideepoki <95339341+saideepoki@users.noreply.github.com>
2024-06-21 15:13:13 +0530

    implemented get newsletters for logged in users

---
+++ src/app/api/getNewsletters/route.ts
@@ -0,0 +1,54 @@
+import { getServerSession } from "next-auth";
+import Newsletter from "@/model/newsletter";
+import { dbConnect } from "@/lib/dbConnect";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { User } from "next-auth";
+
+export async function GET(req: Request) {
+    await dbConnect();
+
+    try {
+    const session = await getServerSession(authOptions);
+
+    if(!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "USer not logged in"
+            },
+            {
+                status: 401
+            }
+        )
+     }
+
+     const newsletters = await Newsletter.aggregate([
+        {$match: {}},
+        {$sort: {'createdAt': -1}}
+     ])
+
+     if(!newsletters || newsletters.length === 0) {
+        return Response.json(
+            {
+                success: false,
+                message: "No newsletters found in database"
+            },
+            {
+                status: 401
+            }
+        )
+     }
+
+     return Response.json(
+        {
+            success: true,
+            message: newsletters
+        },
+        {
+            status: 200
+        }
+     )
+    } catch (error) {
+
+    }
+}
